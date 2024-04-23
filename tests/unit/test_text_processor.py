68ba28e468903ac6305484d4553a3cf8c54c76b0
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-04-23 16:49:56 -0400

    close #96: Add acronym check in `nome_simples` column

--- tests/unit/test_text_processor.py
+++ tests/unit/test_text_processor.py
@@ -1,5 +1,5 @@
 from src.myparser.text_processor import capitalize_nouns_keep_articles_prepositions
-from src.myparser.text_processor import capitalize_text
+from src.myparser.text_processor import capitalize_text, capitalize_text_keep_acronyms
 
 def test_capitalize_nouns_keep_articles_prepositions_1():
     text = "o rato roeu a roupa do rei de roma"
@@ -35,3 +35,21 @@ def test_capitalize_text_3():
     expected = "O Rato Roeu a Roupa do Rei de Roma"
     result = capitalize_text(text)
     assert result != expected
+
+def test_capitalize_text_keep_acronyms_1():
+    text = "this is a test"
+    expected = "This is a test"
+    result = capitalize_text_keep_acronyms(text)
+    assert result == expected
+
+def test_capitalize_text_keep_acronyms_2():
+    text = "this is a TEST"
+    expected = "This is a TEST"
+    result = capitalize_text_keep_acronyms(text)
+    assert result == expected
+
+def test_capitalize_text_keep_acronyms_3():
+    text = ""
+    expected = ""
+    result = capitalize_text_keep_acronyms(text)
+    assert result == expected
