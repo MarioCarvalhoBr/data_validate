3ccc4d201c76187aecfbd209192df40caf1d3347
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-04-26 10:30:22 -0400

    Refactor code to update requirements.txt and fix execution time in log files

--- scripts/generate_logs_coverage_badge.sh
+++ scripts/generate_logs_coverage_badge.sh
@@ -4,12 +4,18 @@
 . ~/miniconda3/etc/profile.d/conda.sh
 
 # ATIVA O AMBIENTE
+echo ""
+echo "Ativando o ambiente..."
 conda activate adapta_data
 
 # GERA O RELATÓRIO DE COBERTURA (badge)
+echo ""
+echo "Gerando o relatório de cobertura..."
 python3 generate_coverage_report.py
 
 # GERANDO OS DADOS PARA OS LOGS
+echo ""
+echo "Gerando os logs..."
 python3 main.py --input_folder=input_data/data_errors_01/ > log/log_errors_01.txt
 python3 main.py --input_folder=input_data/data_errors_02/ > log/log_errors_02.txt
 python3 main.py --input_folder=input_data/data_errors_03/ > log/log_errors_03.txt
@@ -18,13 +24,17 @@ python3 main.py --input_folder=input_data/data_errors_05/ > log/log_errors_05.tx
 python3 main.py --input_folder=input_data/data_ground_truth_01/ > log/log_data_ground_truth_01.txt
 python3 main.py --input_folder=input_data/data_ground_truth_02/ > log/log_data_ground_truth_02.txt
 
-# GERA O RELATÓRIO DE COBERTURA
 # Verificar o status de saída do script
+echo ""
+echo "Verificando o status de saída do script..."
 # Se o script sair com status diferente de zero, o commit será abortado
 if [ $? -eq 0 ]; then
-  echo "Script executado com sucesso. Adicionando image.png ao commit..."
+  echo ""
+  echo "Script executado com sucesso. Adicionando image.png e logs ao staging area do commit."
+
   # Adiciona image.png ao staging area
   git add assets/images/coverage_badge.svg
+
   # Adiciona os logs ao staging area
   git add log/log_errors_01.txt
   git add log/log_errors_02.txt
@@ -33,7 +43,11 @@ if [ $? -eq 0 ]; then
   git add log/log_errors_05.txt
   git add log/log_data_ground_truth_01.txt
   git add log/log_data_ground_truth_02.txt
+
+  echo ""
+  echo "Pipeline finalizado com sucesso."
 else
+  echo ""
   echo "O commit foi abortado devido a falhas nas verificações."
   exit 1
 fi
