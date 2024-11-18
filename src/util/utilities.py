afb052dff8e733f10e20dd222bddc1e357c96947
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-11-18 16:05:24 -0400

    close #199: Adds check to improve messages regarding values ​​ending with punctuation

--- src/util/utilities.py
+++ src/util/utilities.py
@@ -357,7 +357,7 @@ def check_punctuation(df, name_file, columns_dont_punctuation=None, columns_must
                     continue
                 text = str(text).strip()
                 if text[-1] in [',', '.', ';', ':', '!', '?']:
-                    warnings.append(f"{name_file}, linha {index + 2}: A coluna '{column}' não deve terminar com pontuação.")
+                    warnings.append(f"{name_file}, linha {index + 2}: O valor da coluna '{column}' não deve terminar com pontuação.")
         
         if columns_must_end_with_dot is not None:
             for column in columns_must_end_with_dot:
@@ -367,7 +367,7 @@ def check_punctuation(df, name_file, columns_dont_punctuation=None, columns_must
                     continue
                 text = str(text).strip()
                 if text[-1] != '.':
-                    warnings.append(f"{name_file}, linha {index + 2}: A coluna '{column}' deve terminar com ponto.")
+                    warnings.append(f"{name_file}, linha {index + 2}: O valor da coluna '{column}' deve terminar com ponto.")
 
     return not warnings, warnings
 
