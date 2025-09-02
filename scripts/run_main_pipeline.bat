1913a18339dfe269995ee5d7d03eb327567a6318
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2025-09-02 10:30:26 -0400

    feat: enhance README and CI workflows with additional badges and Windows build configuration

--- scripts/run_main_pipeline.bat
+++ scripts/run_main_pipeline.bat
@@ -1,2 +1,3 @@
 @echo off
 poetry run python -m data_validate.main --l=pt_BR --o data/output/ --d --i data/input/data_ground_truth_01/
+poetry run python -m data_validate.main --l=pt_BR --o data/output/ --d --i data/input/data_errors_01/
