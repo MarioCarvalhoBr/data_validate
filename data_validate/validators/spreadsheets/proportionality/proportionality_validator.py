459cb7cfebc023f971eb0e8ff70f8f0f73c98539
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2025-09-11 22:21:18 -0400

    feat: implement new validation methods for proportionality indicators and enhance existing logic

--- data_validate/validators/spreadsheets/proportionality/proportionality_validator.py
+++ data_validate/validators/spreadsheets/proportionality/proportionality_validator.py
@@ -22,6 +22,7 @@ from data_validate.helpers.common.processing.collections_processing import (
     categorize_strings_by_id_pattern_from_list,
     find_differences_in_two_set_with_message,
 )
+from data_validate.helpers.common.processing.collections_processing import generate_group_from_list
 
 
 def get_valids_codes_from_description(
@@ -32,10 +33,10 @@ def get_valids_codes_from_description(
     if column_name_scenario in df_description.columns:
         df_description = df_description[~((df_description[column_name_level] == "2") & (df_description[column_name_scenario] == "0"))]
 
-    codes_cleanned = set(df_description[column_name_code].astype(str))
+    codes_cleaned = set(df_description[column_name_code].astype(str))
     valid_codes = set()
 
-    for code in codes_cleanned:
+    for code in codes_cleaned:
         is_correct, __ = check_cell_integer(code, 1)
         if is_correct:
             valid_codes.add(code)
@@ -222,15 +223,89 @@ class SpProportionalityValidator(ValidatorModelABC):
 
         return errors, warnings
 
-    def validate_bbb(self) -> Tuple[List[str], List[str]]:
+    def validate_columns_repeated_indicators(self) -> Tuple[List[str], List[str]]:
         errors, warnings = [], []
-        print(f"Validating {self.sp_name_proportionality} - BBB")
+
+        local_required_columns = {
+            self.sp_name_proportionality: self.global_required_columns[self.sp_name_proportionality],
+        }
+
+        # Check required columns exist
+        column_errors = self.check_columns_in_models_dataframes(local_required_columns, self.model_dataframes)
+        if column_errors:
+            return column_errors, warnings
+
+        df_proportionalities: DataFrame = self.model_dataframes[self.sp_name_proportionality].copy()
+
+        # Códigos dos indicadores que estão em nível 1
+        level_one_columns = [col for col in df_proportionalities.columns.get_level_values(0).tolist() if not col.lower().startswith("unnamed")]
+        grouped_columns = generate_group_from_list(level_one_columns)
+
+        unique_list = []
+        for group in grouped_columns:
+            first_element = group[0]
+            if first_element not in unique_list:
+                unique_list.append(first_element)
+            else:
+                errors.append(f"{self.sp_name_proportionality}: O indicador pai '{first_element}' está repetido na planilha.")
+
+        errors = list(set(errors))
 
         return errors, warnings
 
-    def validate_ccc(self) -> Tuple[List[str], List[str]]:
+    def validate_relation_indicators_in_value_and_proportionality(self) -> Tuple[List[str], List[str]]:
         errors, warnings = [], []
-        print(f"Validating {self.sp_name_proportionality} - CCC")
+        if self.model_dataframes[self.sp_name_value].empty:
+            self.set_not_executed(
+                [
+                    (
+                        self.validate_relation_indicators_in_value_and_proportionality,
+                        NamesEnum.IND_VAL_PROP.value,
+                    )
+                ]
+            )
+            return errors, warnings
+
+        # Somente com dados de descricao e composicao (deve ser igual, apenas extrair)
+        local_required_columns = {
+            self.sp_name_proportionality: self.global_required_columns[self.sp_name_proportionality],
+            self.sp_name_value: self.global_required_columns[self.sp_name_value],
+        }
+
+        # Check required columns exist
+        column_errors = self.check_columns_in_models_dataframes(local_required_columns, self.model_dataframes)
+        if column_errors:
+            return column_errors, warnings
+
+        df_proportionalities = self.model_dataframes[self.sp_name_proportionality].copy()
+        df_values = self.model_dataframes[self.sp_name_value].copy()
+
+        # Get all codes in proportionality (both levels of MultiIndex)
+
+        # Get all columns in level 1
+        columns_level_one_prop = df_proportionalities.columns.get_level_values(0).unique().tolist()
+        columns_level_one_prop = [col for col in columns_level_one_prop if not col.lower().startswith("unnamed: 0_level_0")]
+
+        # Get all columns in level 2
+        columns_level_two_prop = df_proportionalities.columns.get_level_values(1).unique().tolist()
+        columns_level_two_prop.remove(self.column_name_id)
+
+        # Create a set with all codes in both levels
+        set_all_columns_prop = set(columns_level_one_prop + columns_level_two_prop)
+
+        # Get all codes in values
+        columns_values = df_values.columns.unique().tolist()
+        columns_values.remove(self.column_name_id)
+        set_all_columns_values = set(columns_values)
+
+        # Compare codes between description and proportionality
+        comparison_errors = find_differences_in_two_set_with_message(
+            first_set=set_all_columns_prop,
+            label_1=self.sp_name_proportionality,
+            second_set=set_all_columns_values,
+            label_2=self.sp_name_value,
+        )
+        errors.extend(comparison_errors)
 
         return errors, warnings
 
@@ -251,8 +326,8 @@ class SpProportionalityValidator(ValidatorModelABC):
 
         validations = [
             (self.validate_relation_indicators_in_proportionality, NamesEnum.IR.value),
-            (self.validate_bbb, NamesEnum.IR.value),
-            (self.validate_ccc, NamesEnum.IR.value),
+            (self.validate_columns_repeated_indicators, NamesEnum.REP_IND_PROP.value),
+            (self.validate_relation_indicators_in_value_and_proportionality, NamesEnum.IND_VAL_PROP.value),
             (self.validate_xxx, NamesEnum.IR.value),
             (self.validate_yyy, NamesEnum.IR.value),
         ]
