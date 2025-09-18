8f8abad8ab407e7f92bf6cd67ee8b0317533a9f5
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2025-09-18 18:58:30 -0400

    fix: remove pre-push stage from prepare-pyproject hook in pre-commit configuration

--- data_validate/helpers/base/metadata_info.py
+++ data_validate/helpers/base/metadata_info.py
@@ -13,7 +13,7 @@ class MetadataInfo(ConstantBase):
         project_name: Final = "Canoa"
         dist_name: Final = "canoa_data_validate"
         release_level: Final = "beta"
-        serial: Final = 1079
+        serial: Final = 1080
         status_dev: Final = 10
 
         self.__version__ = "0.0.0"
