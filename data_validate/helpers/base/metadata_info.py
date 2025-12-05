f6507c2746ebb303e6efe8fc7e79187af1ec9112
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2025-12-05 18:09:53 -0400

    chore: add poetry update hook to pre-commit configuration

--- data_validate/helpers/base/metadata_info.py
+++ data_validate/helpers/base/metadata_info.py
@@ -12,7 +12,7 @@ class MetadataInfo(ConstantBase):
         project_name: Final = "Canoa"
         dist_name: Final = "canoa_data_validate"
         release_level: Final = "beta"
-        serial: Final = 674
+        serial: Final = 675
         status_dev: Final = 0
 
         self.__version__ = "0.0.0"
